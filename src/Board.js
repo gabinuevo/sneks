9c63adf45d590200239242d825a13c762f2c2c62
Gabriela <balicas.gb@gmail.com>
2019-08-16 16:50:21 -0700

    refactor board building functionality in the Board component to be outside of the constructor

--- src/Board.js
+++ src/Board.js
@@ -13,16 +13,24 @@ export default class Board extends Component {
       snakeLength: 1,
       loading: true,
       direction: 'right',
-      board: [],
+      board: this.createBoard(),
     }
 
+    
+    this.createBoard = this.createBoard.bind(this);
+    this.autoMoveSnake = this.autoMoveSnake.bind(this);
+    this.manuallyMoveSnake = this.manuallyMoveSnake.bind(this);
+  }
+
+  createBoard() {
+    const board = []
     let snackLocation = Math.floor(Math.random() * 30);
     // if the snack is located at the head of snake
     snackLocation === 15 && snackLocation++;
 
     for (let k = 0; k < 30; k++) {
       if (k !== 15 && k !== snackLocation) {
-        this.state.board.push([...new Array(30).fill(null)]);
+        board.push([...new Array(30).fill(null)]);
       } else {
         let row;
         if (k === snackLocation) {
@@ -34,11 +42,10 @@ export default class Board extends Component {
           left.push('head');
           row = left.concat(new Array(15).fill(null));
         }
-        this.state.board.push(row);
+        board.push(row);
       }
     }
-    this.autoMoveSnake = this.autoMoveSnake.bind(this);
-    this.manuallyMoveSnake = this.manuallyMoveSnake.bind(this);
+    return board;
   }
 
   componentDidMount() {
@@ -58,9 +65,11 @@ export default class Board extends Component {
 
     let i = 0;
     let fullBoard = [];
-    for (let row of board) {
-      const currRow = <Row row={row} key={i} />;
-      fullBoard.push(currRow); i++;
+    if (!loading) {
+      for (let row of board) {
+        const currRow = <Row row={row} key={i} />;
+        fullBoard.push(currRow); i++;
+      }
     }
 
     return (
